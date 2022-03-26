4911b0f15de7642f44d93f6cdeb91d305225ee96
Ujjwal <itsujji@gmail.com>
2022-03-26 22:41:59 +0530

    First commit

---
+++ simpleModel.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.Sequential([
+  keras.layers.Dense(1, activation="relu", input_shape=[1])
+])
+
+sample_input = (1, 2, 3, 4, 5, 6)
+sample_output = (3, 5, 7, 9, 11, 13)
+
+model.compile(optimizer="sgd", loss="mean_squared_error")
+
+# train model
+model.fit(sample_input, sample_output, epochs=500)
+
+print(model.predict([20])) # my output [[17.074446]]
